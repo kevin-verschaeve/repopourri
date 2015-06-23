2d2b1f84cae2b26d87c13afa962c1777f62ac2a4
kevin-verschaeve <kevin.verschaeve@live.fr>
2015-06-23 20:56:17 +0200

    site julien

--- index.php
+++
@@ -1,4 +0,0 @@
-<h1>Prod</h1>
-<?php
-	var_dump($_SERVER);
-?>
--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-
+$(document).ready(function(){
+    $('body').qpScroll({
+        imagesFolder: 'img',
+        imagesArray: [ 'panneau-header.jpg' ],
+        firstSpeed: 5,
+        neighbourRatio: 1.8,
+        offset: 0
+    });
+});
