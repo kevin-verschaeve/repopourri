ce7cc7cd4a69493b9db1a02d9298881db8955aa3
Kevin Verschaeve <kevin.verschaeve@live.fr>
2016-03-30 17:15:26 +0200

    test zsh global alias

--- js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $('body').qpScroll({
-        imagesFolder: 'img',
-        imagesArray: [ 'panneau-header.jpg' ],
-        firstSpeed: 5,
-        neighbourRatio: 1.8,
-        offset: 0
-    });
-});
