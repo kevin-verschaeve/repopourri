e69e72d7b88479008088f7f96fcf69976ef80824
Kevin Verschaeve <kverschaeve@interdrinks.fr>
2021-09-29 13:47:01 +0200

    use origin

--- .github/simple_changelog.sh
+++ .github/simple_changelog.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-hashes=$(git log --no-merges --pretty="format:%h"  master..dev)
+hashes=$(git log --no-merges --pretty="format:%h" origin/master..origin/dev)
 
 commits=''
 for hash in $hashes
 do
-  prId=$(git log --merges --reverse --pretty="format: %s" --ancestry-path $hash..dev | head -n1 | sed -n "s/^.*Merge pull request #\\s*\\([0-9]*\\).*$/\\1/p")
+  prId=$(git log --merges --reverse --pretty="format: %s" --ancestry-path $hash..origin/dev | head -n1 | sed -n "s/^.*Merge pull request #\\s*\\([0-9]*\\).*$/\\1/p")
   commitInfo=$(git show -s --format="**%aN** - *%s* (commit: [#%h](https://github.com/kevin-verschaeve/repopourri/commit/%h), " $hash)
   commits=$(echo $commits$commitInfo 'PR: [#'$prId'](https://github.com/kevin-verschaeve/repopourri/pull/'$prId'/files))'"\n")
 done
