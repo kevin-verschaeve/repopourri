85c7e4387c4cc43f7b5deb312886841e42e50d87
Kevin Verschaeve <kverschaeve@interdrinks.fr>
2021-09-29 13:31:13 +0200

    update script to send json

--- .github/simple_changelog.sh
+++ .github/simple_changelog.sh
@@ -2,9 +2,12 @@
 
 hashes=$(git log --no-merges --pretty="format:%h"  master..dev)
 
+commits=''
 for hash in $hashes
 do
   prId=$(git log --merges --reverse --pretty="format: %s" --ancestry-path $hash..dev | head -n1 | sed -n "s/^.*Merge pull request #\\s*\\([0-9]*\\).*$/\\1/p")
   commitInfo=$(git show -s --format="**%aN** - *%s* (commit: [#%h](https://github.com/kevin-verschaeve/repopourri/commit/%h), " $hash)
-  echo $commitInfo 'PR: [#'$prId'](https://github.com/kevin-verschaeve/repopourri/pull/'$prId'/files))'
+  commits=$(echo $commits$commitInfo 'PR: [#'$prId'](https://github.com/kevin-verschaeve/repopourri/pull/'$prId'/files))'"\n")
 done
+
+echo '{"body": "'$commits'"}'
